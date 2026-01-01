d394fcfc335151a87c3b232e075f38f679d73ad9
Matthew Blott <m4ttb@pm.me>
2026-01-01 15:39:33 +0000

    saved

--- App/Configuration/Endpoint.swift
+++ App/Configuration/Endpoint.swift
@@ -3,9 +3,8 @@ import UIKit
 struct Endpoint {
   static let remote: URL? = URL(string: "https://scribble.coderscoffeehouse.com")!
   static let local: URL? = URL(string: "http://localhost:3000")!
-//  static let local: URL? = URL(string: "https://canniest-colt-undeeply.ngrok-free.dev")!
 
   static var baseURL: URL? {
-    local
+    remote
   }
 }
